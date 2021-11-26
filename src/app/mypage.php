bf0eea9524accfa4fcf891a219e0c1c65895ba66
kawada68 <nodoguro68@gmail.com>
2021-11-26 17:21:46 +0900

    ログアウト機能作成

---
+++ src/app/mypage.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../common/common.php';
+require_once '../function/user.php';
+
+
+
+$page_title = 'マイページ';
+include '../template/head.php';
+include '../template/header.php';
+?>
+
+<main class="main">
+    <div class="container">
+
+    </div>
+
+</main>
+<?php include '../template/footer.php' ?>
+
+</body>
+
+</html>
