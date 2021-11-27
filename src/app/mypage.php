436f4b9f8bb2d523368f56049fb5e7781090d41b
kawada68 <nodoguro68@gmail.com>
2021-11-27 17:46:13 +0900

    フォルダ削除機能作成

--- src/app/mypage.php
+++ src/app/mypage.php
@@ -9,6 +9,14 @@ require_once '../function/folder.php';
 $user_id = $_SESSION['user_id'];
 $folders = getFolders($user_id);
 
+if (!empty($_GET['folder_id'])) {
+
+    $folder_id = filter_input(INPUT_GET, 'folder_id');
+    $folder = getFolder($folder_id, $user_id);
+    $delete_folder_id = $folder['id'];
+    $selected_folder_title = $folder['title'];
+}
+
 if (!empty($_POST['create_folder'])) {
 
     $folder = trim(filter_input(INPUT_POST, 'create_folder'));
@@ -18,12 +26,22 @@ if (!empty($_POST['create_folder'])) {
 
     if (empty($err_msg)) {
 
-        createFolder($user_id, $folder);
+        $folder_id = createFolder($user_id, $folder);
 
-        header('Location: mypage.php');
+        if(!empty($folder_id)) {
+            header('Location: mypage.php?folder_id='.$folder_id);
+        }
     }
 }
 
+if (!empty($_POST['delete_folder'])) {
+
+    $folder_id = filter_input(INPUT_POST, 'delete_folder');
+
+    deleteFolder($folder_id, $user_id);
+
+    header('Location: mypage.php');
+}
 
 $page_title = 'マイページ';
 include '../template/head.php';
@@ -51,6 +69,21 @@ include '../template/header.php';
             <input type="submit" value="＋">
             <span class="err-msg"><?= getErrMsg('create_folder'); ?></span>
         </form>
+
+        <section class="section">
+            <div class="section__header">
+                <?php if (!empty($selected_folder_title)) : ?>
+                    <h2 class=""><?= sanitize($selected_folder_title); ?></h2>
+                    <form method="post" class="">
+                        <button type="submit" name="delete_folder" value="<?= sanitize($delete_folder_id); ?>">削除</button>
+                    </form>
+                <?php else : ?>
+                    <h2>フォルダが選択されていません</h2>
+                <?php endif; ?>
+            </div>
+            <div class="section__body"></div>
+            <div class="section__footer"></div>
+        </section>
     </div>
 
 </main>
--- src/function/folder.php
+++ src/function/folder.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // フォルダ新規登録
 function createFolder($user_id, $folder)
@@ -14,7 +14,7 @@ function createFolder($user_id, $folder)
         );
 
         if (queryPost($dbh, $sql, $data)) {
-            return;
+            return $dbh->lastInsertId();
         }
     } catch (Exception $e) {
         error_log('エラー発生:' . $e->getMessage());
@@ -36,6 +36,52 @@ function getFolders($user_id)
         $stmt = queryPost($dbh, $sql, $data);
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $result;
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
+
+
+// フォルダ取得
+function getFolder($folder_id, $user_id)
+{
+    try {
+
+        $dbh = dbConnect();
+
+        $sql = 'SELECT id, title FROM folders WHERE id = :folder_id AND user_id = :user_id AND is_deleted = 0';
+        $data = array(
+            ':folder_id' => $folder_id,
+            ':user_id' => $user_id
+        );
+
+        $stmt = queryPost($dbh, $sql, $data);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = ERR_MSG;
+    }
+}
+
+
+// フォルダ削除
+function deleteFolder($folder_id, $user_id)
+{
+    try {
+
+        $dbh = dbConnect();
+
+        $sql = 'UPDATE folders SET is_deleted = 1 WHERE id = :folder_id AND user_id = :user_id AND is_deleted = 0';
+        $data = array(
+            ':folder_id' => $folder_id,
+            ':user_id' => $user_id,
+        );
+
+        if (queryPost($dbh, $sql, $data)) {
+            return true;
+        }
     } catch (Exception $e) {
         error_log('エラー発生:' . $e->getMessage());
         $err_msg['common'] = ERR_MSG;
