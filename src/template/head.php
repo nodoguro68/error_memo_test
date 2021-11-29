3891f1c76cf2acba065a4bec042e6d41ecaee189
kawada68 <nodoguro68@gmail.com>
2021-11-29 19:06:41 +0900

    css読み込み

--- src/template/head.php
+++ src/template/head.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title; ?></title>
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="../resource/css/style.css">
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
